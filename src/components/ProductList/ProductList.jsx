cd017632df55806c8f0400b00491a1a9a2825574
Sergey <seregaland@yandex.ru>
2022-10-26 16:01:04 +0300

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -31,7 +31,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('http://85.119.146.179:8000/web-data', {
+        fetch('http://5.159.101.233:8000/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
