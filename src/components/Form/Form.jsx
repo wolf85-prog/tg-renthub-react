3d30b650951a099bea3820fb13379f6542e03ae7
Sergey <seregaland@yandex.ru>
2022-10-26 17:10:49 +0300

    init

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -52,7 +52,7 @@ const Form = () => {
 
     return (
         <div className={"form"}>
-            <h3>Введите ваши данные</h3>
+            <h3>Введите данные, чтобы создать проект</h3>
             <input
                 className={'input'}
                 type="text"
