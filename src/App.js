876e04c555bc6cf672dac049b080452d77ced355
Sergey <seregaland@yandex.ru>
2022-10-26 16:57:50 +0300

    init

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ function App() {
         <div className="App">
             <Header />
             <Routes>
-                <Route index element={<Form />}/>
-                <Route path={'form'} element={<ProductList />}/>
+                <Route index element={<ProductList />}/>
+                <Route path={'form'} element={<Form />}/>
             </Routes>
         </div>
     );
