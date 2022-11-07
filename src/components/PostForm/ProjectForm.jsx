78540e6b8577ad925516ec64976bc94d3b267b11
Sergey <seregaland@yandex.ru>
2022-11-07 16:56:00 +0300

    select update

--- src/components/PostForm/ProjectForm.jsx
+++ src/components/PostForm/ProjectForm.jsx
@@ -1,8 +1,11 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import MyButton from "../UI/MyButton/MyButton";
 import './ProjectForm.css';
+import ButtonPlus from "../../img/plus.png";
+import ButtonMinus from "../../img/minus.png";
+import Comp1 from "../../img/spec/comp1.svg"
 import OutlinedInput from '@mui/material/OutlinedInput';
-import {FormControl, IconButton, InputAdornment, InputLabel, MenuItem, Select} from "@mui/material";
+import {FormControl, Grid, IconButton, InputAdornment, InputBase, InputLabel, MenuItem, Select} from "@mui/material";
 import NearMeIcon from '@mui/icons-material/NearMe';
 import dayjs from 'dayjs';
 import Stack from '@mui/material/Stack';
@@ -13,30 +16,291 @@ import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
+import { alpha, styled } from '@mui/material/styles';
+import { AccessAlarm, ThreeDRotation } from '@mui/icons-material';
+import Typography from '@mui/material/Typography';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 
-const categories = [
+const RedditTextField = styled((props) => (
+    <TextField InputProps={{ disableUnderline: true }} {...props} />
+))(({ theme }) => ({
+    '& .MuiFilledInput-root': {
+        border: '1px solid #e2e2e1',
+        overflow: 'hidden',
+        borderRadius: 4,
+        backgroundColor: theme.palette.mode === 'light' ? '#fcfcfb' : '#2b2b2b',
+        transition: theme.transitions.create([
+            'border-color',
+            'background-color',
+            'box-shadow',
+        ]),
+        '&:hover': {
+            backgroundColor: 'transparent',
+        },
+        '&.Mui-focused': {
+            backgroundColor: 'transparent',
+            boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 2px`,
+            borderColor: theme.palette.primary.main,
+        },
+    },
+}));
+
+const BootstrapInput = styled(InputBase)(({ theme }) => ({
+    'label + &': {
+        marginTop: theme.spacing(3),
+    },
+    '& .MuiInputBase-input': {
+        borderRadius: 4,
+        position: 'relative',
+        backgroundColor: theme.palette.mode === 'light' ? '#fcfcfb' : '#2b2b2b',
+        border: '1px solid #ced4da',
+        fontSize: 16,
+        width: 'auto',
+        padding: '10px 12px',
+        transition: theme.transitions.create([
+            'border-color',
+            'background-color',
+            'box-shadow',
+        ]),
+        // Use the system font instead of the default Roboto font.
+        fontFamily: [
+            '-apple-system',
+            'BlinkMacSystemFont',
+            '"Segoe UI"',
+            'Roboto',
+            '"Helvetica Neue"',
+            'Arial',
+            'sans-serif',
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
+        ].join(','),
+        '&:focus': {
+            boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 0.2rem`,
+            borderColor: theme.palette.primary.main,
+        },
+    },
+}));
+
+
+const data = [
     {
-        value: 'SOUND',
-        label: 'SOUND',
+        id: 1,
+        name: 'SOUND',
+        models: [
+            {
+                id: 1,
+                name: 'Звукорежиссер',
+            },
+            {
+                id: 2,
+                name: 'RF менеджер',
+            },
+            {
+                id: 3,
+                name: 'Backline',
+            },
+            {
+                id: 4,
+                name: 'Roadie',
+            },
+            {
+                id: 5,
+                name: 'Техник по звуку',
+            },
+        ]
     },
     {
-        value: 'LIGHT',
-        label: 'LIGHT',
+        id: 2,
+        name: 'LIGHT',
+        models: [
+            {
+                id: 1,
+                name: 'Художник по свету',
+            },
+            {
+                id: 2,
+                name: 'Оператор световой пушки',
+            },
+            {
+                id: 3,
+                name: 'Гафер',
+            },
+            {
+                id: 4,
+                name: 'Техник по свету',
+            },
+        ]
     },
     {
-        value: 'VIDEO',
-        label: 'VIDEO',
+        id: 3,
+        name: 'VIDEO',
+        models: [
+            {
+                id: 1,
+                name: 'Инженер VMix',
+            },
+            {
+                id: 2,
+                name: 'Инженер Resolume',
+            },
+            {
+                id: 3,
+                name: 'Инженер Zoom',
+            },
+            {
+                id: 4,
+                name: 'Оператор [сameraman]',
+            },
+            {
+                id: 5,
+                name: 'Гафер',
+            },
+            {
+                id: 6,
+                name: 'IT-специалист',
+            },
+            {
+                id: 7,
+                name: 'Техник монтажа',
+            },
+        ]
     },
     {
-        value: 'RIGGERS',
-        label: 'RIGGERS',
+        id: 4,
+        name: 'RIGGERS',
+        models: [
+            {
+                id: 1,
+                name: 'High Rigger [???]',
+            },
+            {
+                id: 2,
+                name: 'Lo Rigger [???]',
+            },
+        ]
     },
+    {
+        id: 5,
+        name: 'STAGEHANDS',
+        models: [
+            {
+                id: 1,
+                name: 'Погрузка / разгрузка',
+            },
+            {
+                id: 2,
+                name: 'Монтаж / демонтаж',
+            },
+        ]
+    },
+    {
+        id: 6,
+        name: 'STAGE GROUND',
+        models: [
+            {
+                id: 1,
+                name: 'High Rigger [???]',
+            },
+            {
+                id: 2,
+                name: 'Lo Rigger [???]',
+            },
+        ]
+    },
+    {
+        id: 7,
+        name: 'TRUCKS',
+        models: [
+            {
+                id: 1,
+                name: 'C личным ТС [B]',
+            },
+            {
+                id: 2,
+                name: 'Без личного ТС [B]',
+            },
+            {
+                id: 3,
+                name: 'С гидролифтом',
+            },
+            {
+                id: 4,
+                name: 'Без гидролифта',
+            },
+            {
+                id: 5,
+                name: 'Грузоподъемность 4 т.',
+            },
+            {
+                id: 6,
+                name: 'Грузоподъемность 6 т.',
+            },
+            {
+                id: 7,
+                name: 'Грузоподъемность 7 т.',
+            },
+            {
+                id: 8,
+                name: 'Грузоподъемность 8 т.',
+            },
+            {
+                id: 9,
+                name: 'Грузоподъемность 10 т.',
+            },
+            {
+                id: 10,
+                name: 'Грузоподъемность 14 т.',
+            },
+        ]
+    },
+    {
+        id: 8,
+        name: 'PRODUCTION',
+        models: [
+            {
+                id: 1,
+                name: 'Мероприятие под ключ',
+            },
+            {
+                id: 2,
+                name: 'Отдельные технические задачи',
+            },
+        ]
+    }
 ];
+
+// компонент пользовательского выпадающего списка
+const CustomSelect = ({ id, options, onChange }) => {
+    return (
+        <TextField fullWidth
+                   id={id}
+                   select
+                   label="Категория"
+                   onChange={onChange}>
+            {options.map((option, index) => (
+                <MenuItem key={id + index} value={option.id}>
+                    {option.name}
+                </MenuItem>
+            ))}
+        </TextField>
+
+    /*<select className="custom-select" id={id} onChange={onChange}>
+            { options.map((option, index) =>
+                <option key={id + index} value={option.id}>
+                    {option.name}
+                </option>
+            )}
+        </select>*/
+    )
+}
 
 
 const ProjectForm = ({create}) => {
     const [post, setPost] = useState({title: '', body: ''})
+    const [categories, setCategories] = useState([]); // хранилище категорий
+    const [models, setModels] = useState([]);         // хранилище моделей
 
     const addNewProject = (e) => {
         e.preventDefault();
@@ -47,7 +311,6 @@ const ProjectForm = ({create}) => {
         setPost({title: '', body: ''})
     }
 
-    const [category, setCategory] = React.useState('AUDIO');
 
     const [value, setValue] = React.useState(dayjs('2022-01-01T0:0:54'));
 
@@ -60,6 +323,35 @@ const ProjectForm = ({create}) => {
             console.log("Latitude is :", position.coords.latitude);
             console.log("Longitude is :", position.coords.longitude);
         });
+    }
+
+    // при первой загрузке приложения выполнится код ниже
+    useEffect(() => {
+
+        // устанавливаем категории
+        setCategories(data.legnth > 0 && data);
+
+        // и модели из первой категории по умолчанию
+        setModels(data.length > 0 && data[0].models && data[0].models.length > 0);
+
+    }, []);
+
+    // при выборе нового значения в категории
+    const onCategoriesSelectChange = (e) => {
+
+        // преобразуем выбранное значение опции списка в число - идентификатор категории
+        const categoryId = parseInt(e.target.options[e.target.selectedIndex].value);
+
+        // получаем из массива категорий объект категории по соответствующему идентификатору
+        const category = categories.find(item => item.id === categoryId);
+
+        // выбираем все модели в категории, если таковые есть
+        const models = category.models && category.models.length > 0
+            ? category.models
+            : [{ id: 0, name: 'Нет моделей', items: [] }];
+
+        // меняем модели во втором списке
+        setModels(models);
     }
 
     return (
@@ -91,11 +383,13 @@ const ProjectForm = ({create}) => {
                 <label className="text-field__label" htmlFor="date">Укажите геолокацию</label>
             </div>
             */}
+
             <div className="text-field text-field_floating">
-                <TextField fullWidth
-                           id="outlined-basic"
+                <RedditTextField fullWidth
                     label="Название проекта"
-                    variant="outlined"
+                    defaultValue="react-reddit"
+                    id="reddit-input"
+                    variant="filled"
                 />
             </div>
 
@@ -129,7 +423,7 @@ const ProjectForm = ({create}) => {
             </div>
 
             <div className="text-field text-field_floating">
-                <TextField fullWidth
+                <RedditTextField fullWidth
                     id="outlined-multiline-flexible"
                     label="Техническое задание"
                     multiline
@@ -141,25 +435,25 @@ const ProjectForm = ({create}) => {
                 <label>
                     <p>Добавьте специалистов</p>
                     <div className="text-field text-field_floating">
-                        <TextField fullWidth
-                            id="outlined-select-currency"
-                            select
-                            label="Категория"
-                        >
-                            {categories.map((option) => (
-                                <MenuItem key={option.value} value={option.value}>
-                                    {option.label}
-                                </MenuItem>
-                            ))}
-                        </TextField>
+                        <label htmlFor="category">Категории</label>
+                        <CustomSelect
+                            id="category"
+                            options={data}
+                            onChange={onCategoriesSelectChange}
+                        />
+
                     </div>
                     <div>
+                        {/*<CustomSelect
+                            id="model"
+                            options={models}
+                        />*/}
                         <TextField fullWidth
                             id="outlined-select-currency"
                             select
                             label="Специальность"
                         >
-                            {categories.map((option) => (
+                            {data.map((option) => (
                                 <MenuItem key={option.value} value={option.value}>
                                     {option.label}
                                 </MenuItem>
@@ -168,7 +462,79 @@ const ProjectForm = ({create}) => {
                     </div>
 
                 </label>
-                <p><MyButton>Добавить</MyButton></p>
+
+                <p style={{marginTop: "15px"}}>
+                    Количество
+                </p>
+                <div className={'kol_group'}>
+                    <img src={ButtonMinus} alt='Минус'/>
+                    <FormControl sx={{ m: 1, width: '65px' , height: '43px'}} variant="outlined">
+                        <OutlinedInput
+                            id="outlined-adornment-weight"
+                            aria-describedby="outlined-weight-helper-text"
+                            inputProps={{
+                                'aria-label': 'weight',
+                            }}
+                        />
+                    </FormControl>
+                    <img src={ButtonPlus} alt='Плюс'/>
+                </div>
+
+                <p><MyButton style={{width: "103px", marginBottom: "15px"}}>Добавить</MyButton></p>
+            </div>
+
+            <div className={'list_spec'}>
+                <img style={{marginTop: "24px"}} src={Comp1} alt='Comp1'/>
+
+                <FormControl sx={{marginLeft: '10px'}} variant="standard">
+                    <InputLabel shrink htmlFor="bootstrap-input">
+                        Sound
+                    </InputLabel>
+                    <BootstrapInput defaultValue="Звукорежессер" id="bootstrap-input" />
+                </FormControl>
+
+                <FormControl sx={{marginLeft: '5px', width: '35px' }} variant="standard">
+                    <InputLabel shrink htmlFor="bootstrap-input" />
+                    <BootstrapInput defaultValue="1" id="bootstrap-input" />
+                </FormControl>
+
+                <DeleteIcon style={{marginBottom: "10px", marginLeft: "10px"}} />
+            </div>
+
+            <div className={'list_spec'}>
+                <img style={{marginTop: "24px"}} src={Comp1} alt='Comp1'/>
+
+                <FormControl sx={{marginLeft: '10px'}} variant="standard">
+                    <InputLabel shrink htmlFor="bootstrap-input">
+                        Sound
+                    </InputLabel>
+                    <BootstrapInput defaultValue="Звукорежессер" id="bootstrap-input" />
+                </FormControl>
+
+                <FormControl sx={{marginLeft: '5px', width: '35px' }} variant="standard">
+                    <InputLabel shrink htmlFor="bootstrap-input" />
+                    <BootstrapInput defaultValue="1" id="bootstrap-input" />
+                </FormControl>
+
+                <DeleteIcon style={{marginBottom: "10px", marginLeft: "10px"}} />
+            </div>
+
+            <div className={'list_spec'}>
+                <img style={{marginTop: "24px"}} src={Comp1} alt='Comp1'/>
+
+                <FormControl sx={{marginLeft: '10px'}} variant="standard">
+                    <InputLabel shrink htmlFor="bootstrap-input">
+                        Sound
+                    </InputLabel>
+                    <BootstrapInput defaultValue="Звукорежессер" id="bootstrap-input" />
+                </FormControl>
+
+                <FormControl sx={{marginLeft: '5px', width: '35px' }} variant="standard">
+                    <InputLabel shrink htmlFor="bootstrap-input" />
+                    <BootstrapInput defaultValue="1" id="bootstrap-input" />
+                </FormControl>
+
+                <DeleteIcon style={{marginBottom: "10px", marginLeft: "10px"}} />
             </div>
 
             <MyButton onClick={addNewProject}>Создать проект</MyButton>
