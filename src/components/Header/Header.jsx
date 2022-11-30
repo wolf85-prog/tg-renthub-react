707d00fa85e05d48d9a7b3ff0e5d8021869c5177
Sergey <seregaland@yandex.ru>
2022-11-30 11:28:34 +0300

    username

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -21,7 +21,7 @@ const Header = (props) => {
             </span>
 
             <span className={'username'}>
-                {user?.id}
+                {user?.username}
             </span>
             <img className={'btn-close'} onClick={onClose} src={CloseButton}/>
         </div>
