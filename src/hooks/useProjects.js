eddbe78d737cc9d960f260329f07161fa39d671f
Sergey <seregaland@yandex.ru>
2022-12-01 17:19:55 +0300

    fix

--- src/hooks/useProjects.js
+++ src/hooks/useProjects.js
@@ -15,18 +15,17 @@ export const useProjects = (posts, manager, sort, query) => {
     //const sortedPosts = useSortedPosts(posts, sort);
 
     const sortedAndSearchedPosts = useMemo(() => {
-        if (manager != '' )  {
-            const filterPosts = posts.filter(post => (post.manager != null ? post.manager : '') === manager)
         
-            if (query != '') {
-                if (query == 'All') {
-                    return filterPosts;
-                }
-                return filterPosts.filter(post => (post.status_id != null ? post.status_id.name : '') === query)
+        const filterPosts = posts.filter(post => (post.manager != null ? post.manager : '') === manager)
+        
+        if (query != '') {
+            if (query == 'All') {
+                return filterPosts;
             }
-            return filterPosts
-        }       
+            return filterPosts.filter(post => (post.status_id != null ? post.status_id.name : '') === query)
+        }
         
+        return filterPosts
      }, [query, posts])
 
      return sortedAndSearchedPosts;
