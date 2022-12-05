b45b3d4c16c51970f8abc3905e6aeb226af6ecc4
Sergey <seregaland@yandex.ru>
2022-12-05 17:08:24 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ function App() {
 
                 arrayPost.push(newPost2)
                 
-                //console.log('arrayPost: ', arrayPost) 
+                console.log('arrayPost: ', arrayPost) 
                 
             })
 
