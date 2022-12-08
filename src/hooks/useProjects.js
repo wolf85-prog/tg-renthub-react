b096dd51852e7ae34128d92ad4568d0311b540e6
Sergey <seregaland@yandex.ru>
2022-12-08 16:18:52 +0300

    set filterpost manager_id

--- src/hooks/useProjects.js
+++ src/hooks/useProjects.js
@@ -11,31 +11,32 @@ import {useMemo} from "react";
 //     return sortedPosts;
 // }
 
-export const useProjects = (posts2, manager, sort, query) => {
+export const useProjects = (posts, manager, sort, query) => {
     //const sortedPosts = useSortedPosts(posts, sort);
 
-    console.log('posts: ', posts2)
+    console.log('posts: ', posts)
 
     const sortedAndSearchedPosts = useMemo(() => {
         
-        // const filterPosts = posts.filter(post => (post.manager != null ? post.manager : '') === manager)
+        const filterPosts = posts.filter(post => (post.manager != null ? post.manager : '') === manager)
         
-        // if (query != '') {
-        //     if (query == 'All') {
-        //         return filterPosts;
-        //     }
-        //     return filterPosts.filter(post => (post.status_id != null ? post.status_id.name : '') === query)
-        // }
-        // return filterPosts
-
         if (query != '') {
             if (query == 'All') {
-                return posts2;
+                return filterPosts;
             }
-            return posts2.filter(post => (post.status_id != null ? post.status_id.name : '') === query)
+            return filterPosts.filter(post => (post.status_id != null ? post.status_id.name : '') === query)
         }
-        return posts2
-     }, [query, posts2])
+        return filterPosts
+
+        // if (query != '') {
+        //     if (query == 'All') {
+        //         return posts2;
+        //     }
+        //     return posts2.filter(post => (post.status_id != null ? post.status_id.name : '') === query)
+        // }
+        // return posts2
+        
+     }, [query, filterPosts])
 
      return sortedAndSearchedPosts;
 }
