3a59b45f3fef9ea2092673d5f8851d7759717e9b
seregey gornih <you@example.com>
2023-04-20 19:26:57 +0300

    fix date

--- src/components/ProjectItem/ProjectItem.jsx
+++ src/components/ProjectItem/ProjectItem.jsx
@@ -36,7 +36,7 @@ const ProjectItem = (props) => {
     const chas = String(d.getHours()).padStart(2, "0"); //d.getHours();
     const minut = String(d.getMinutes()).padStart(2, "0"); //d.getMinutes();
     const monthName = months[d.getMonth()];      
-    const formatted = (d2) ? `${date} ${monthName} ${year} ${chas}:${minut}` : `${date} ${monthName} ${year}`;
+    const formatted = (d2) ? `${date} ${monthName} ${chas}:${minut}` : `${date} ${monthName}`;
 
     //time end
     const year2 = d_end.getFullYear()
@@ -44,7 +44,7 @@ const ProjectItem = (props) => {
     const chas2 = String(d_end.getHours()).padStart(2, "0"); //d.getHours();
     const minut2 = String(d_end.getMinutes()).padStart(2, "0"); //d.getMinutes();
     const monthName2 = months[d_end.getMonth()];       
-    const formatted2 = `${date2} ${monthName2} ${year2}`;
+    const formatted2 = `${date2} ${monthName2}`;
 
 
     Object.values(props.post.workers).map((worker) =>
@@ -60,7 +60,7 @@ const ProjectItem = (props) => {
             <div className="post__content">
                 <div className="post_title">{props.post.title}</div>
                 <div className="subscribe">
-                    {formatted}{formatted2 ? ' - '+ formatted2 : ''}
+                    {formatted}{formatted2 ? ' - '+ formatted2 : ''} {year}
                 </div>
 
                 <ProjectWorkList workers={props.post.workers} defould={count}/>
