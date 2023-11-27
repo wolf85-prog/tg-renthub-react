6d36f2ebb5c8795574c6b0263c25447833449fe9
Sergey <seregaland@yandex.ru>
2023-11-27 10:38:31 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
+import { UserProvider } from './contexts/UserContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
       <React.StrictMode>
-          <BrowserRouter>
+         <UserProvider>
+            <BrowserRouter>
                 <App />
-          </BrowserRouter>
+            </BrowserRouter>
+         </UserProvider> 
       </React.StrictMode>
 );
 
