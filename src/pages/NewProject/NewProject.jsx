e1c9aa315579ab7cce01ef345699a4e5eb06baa2
Sergey <seregaland@yandex.ru>
2023-12-21 16:37:40 +0300

    fix

--- src/pages/NewProject/NewProject.jsx
+++ src/pages/NewProject/NewProject.jsx
@@ -134,7 +134,7 @@ const NewProject = () => {
                 console.log("Менеджер не найден!")
                 
                 //поиск менеджера в notion
-                const managerId = await getManagerIdApi(user?.id)
+                const managerId = await getManagerIdApi((user?.id).toString())
                 console.log("Менеджер из ноушен: ", managerId)
                 
                 //если менеджер не найден, то создать в notion
