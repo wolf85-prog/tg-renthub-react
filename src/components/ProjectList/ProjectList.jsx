5b33fc5492471e66419998569f682c9ba382101f
Sergey <seregaland@yandex.ru>
2024-05-18 15:12:11 +0300

    fix text

--- src/components/ProjectList/ProjectList.jsx
+++ src/components/ProjectList/ProjectList.jsx
@@ -8,7 +8,7 @@ const ProjectList = ({posts, title, remove}) => {
     if (!posts.length) {
         return (
             <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', height: '70vh', zIndex: 10, position: 'relative'}}>
-                <h2>Проекты не найдены</h2>
+                <h2>Для создания первой заявки нажмите на кнопку внизу</h2>
             </div>       
         )
     } else {
