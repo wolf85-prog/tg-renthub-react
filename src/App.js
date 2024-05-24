8da4673e02388850ce2532fe2feb946d7e0b891e
Sergey <seregaland@yandex.ru>
2024-05-24 14:40:41 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Load from "./pages/Load/Load";
 import Posts from "./pages/Posts/Posts";
 import NewProject from "./pages/NewProject/NewProject";
+import NewStavka from "./pages/NewStavka/NewStavka";
 
 function App() { 
 
@@ -24,6 +25,7 @@ function App() {
                     <Route index element={<Load />}/>
                     <Route path={'posts'} element={<Posts />}/>
                     <Route path={'add-project'} element={<NewProject />}/>
+                    <Route path={'add-stavka/:id'} element={<NewStavka />}/>
                 </Routes>
         </ThemeProvider>
     );
---
+++ src/pages/NewStavka/NewStavka.jsx
@@ -0,0 +1,120 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import {useParams} from "react-router-dom";
+import {useTelegram} from "../../hooks/useTelegram";
+import Header from "../../components/Header/Header";
+import './NewStavka.css';
+import CurrencyInput from './../../components/CurrencyInput'
+
+import BlackFon from "./../../img/fon_grad.svg";
+
+const API_URL = process.env.REACT_APP_API_URL
+
+const NewStavka = () => {
+    const { id } = useParams();
+    const {tg, queryId, user} = useTelegram();
+
+    const [summaStavki, setSummaStavki] = useState()
+
+    const [widthD, setWidthD] = useState(0)
+
+
+//----------------------------------------------------------------------------------
+
+
+    const changeSummaStavki = (e) => {
+        console.log(e.target.value)
+
+        setSummaStavki(e.target.value)
+    }
+
+    //отправка данных в telegram-бот
+    const onSendData = useCallback(() => {
+        const data = {
+            summaStavki,
+            id,
+            queryId,
+            userId: user?.id,
+        }
+
+        tg.MainButton.hide();
+
+        fetch(API_URL + 'web-stavka', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        })
+        
+              
+    }, [summaStavki, id])
+
+
+    useEffect(()=>{
+        tg.setHeaderColor('#343A41') // установка цвета хедера
+        tg.setBackgroundColor('#26292c') // установка цвета бэкграунда
+        
+        if (!tg.isExpanded) {
+           tg.expand() //раскрыть приложение на всю высоту 
+        }
+        
+    }, [])
+
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData)
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
+    }, [onSendData])
+
+    useEffect(() => {
+        tg.MainButton.setParams({
+            text: 'Отправить предложение',
+            color: '#000000' //'#2e2e2e'
+        })
+    }, [])
+
+    useEffect(() => {
+        if (summaStavki) {
+           tg.MainButton.show(); 
+        } else {
+            tg.MainButton.hide();  
+        }
+        
+    }, [summaStavki])
+
+
+
+    //---------------------------------------------------------------------------------------
+
+    return (
+        <div className="App">
+            <Header header={{title: 'Моё предложение', icon: 'false'}}/>
+
+            {/* темный фон */}
+            <img src={BlackFon} alt='' className='fon-black' />
+            <img src={BlackFon} alt='' className='fon-black' style={{left: `${widthD}px`, zIndex: '1'}} />
+
+            <div style={{height: '100vh', marginTop: '50%'}}>
+                <div className='form-edit-stavka'>
+                    
+                    <div className='rec1-input'></div>
+                    <div className='rec2-input'></div>
+                    <div className='rec3-input'></div>
+                        <CurrencyInput
+                            className='input-style3'
+                            placeholder='Впиши сюда сумму'
+                            type="text"
+                            value={summaStavki}
+                            onChange={changeSummaStavki} 
+                        /> 
+    
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+
+export default NewStavka;
