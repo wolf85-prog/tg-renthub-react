c5c33bf89cb79f5c21796e196866df4261a5523d
Sergey Gornih <seregaland@yandex.ru>
2024-09-10 13:17:08 +0300

    fix

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -9,6 +9,7 @@ import { useNavigate } from "react-router-dom";
 import Header from "../../components/Header/Header";
 import Loader from "../../components/UI/Loader/Loader";
 import './Error.css';
+import MyModal from "../../components/MyModal/MyModal";
 
 import { getManagerIdApi, getProjectsCashApi } from '../../http/projectAPI';
 
@@ -23,6 +24,7 @@ function Error() {
     const [managerId, setManagerId] = useState("");
 	const [projects2, setProjects2] = useState([]);
 	const [status, setStatus] = useState([]);
+    const [showModal, setShowModal] = useState(true)
 
 	const arr_status = [] 
 
@@ -35,21 +37,21 @@ function Error() {
 //---------------------------------------------------------------------------------------
 
 	// при первой загрузке приложения выполнится код ниже
-    useEffect(() => {
-        setIsPostsLoading(true)
-    }, []);
+    // useEffect(() => {
+    //     setIsPostsLoading(true)
+    // }, []);
 
-    useEffect(() => {
-        if (projects.length > 0) {
-            setProjects2(projects)
-            setIsPostsLoading(false) 
-        }
+    // useEffect(() => {
+    //     if (projects.length > 0) {
+    //         setProjects2(projects)
+    //         setIsPostsLoading(false) 
+    //     }
 
-        setTimeout(()=> {
-            setIsPostsLoading(false) 
-        }, 2000)
+    //     setTimeout(()=> {
+    //         setIsPostsLoading(false) 
+    //     }, 2000)
         
-    }, [projects]);
+    // }, [projects]);
  
 
 //----------start--------------------------------------------------------------------------
@@ -89,7 +91,7 @@ function Error() {
     return (
         <div className="App">
 
-            <Header header={{title: 'Мои проекты', icon: 'false'}}/>
+            {/* <Header header={{title: 'Мои проекты', icon: 'false'}}/>
 
 
             <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', height: '70vh', zIndex: 10, position: 'relative'}}>
@@ -100,8 +102,23 @@ function Error() {
                     <h2>Напишите ваш вопрос в чат</h2>
                 </div>
                 
-            </div> 
+            </div>  */}
 
+            <MyModal visible={showModal} setVisible={setShowModal}>
+                <div className='info-card' style={{height: '220px'}}>
+                    <div className='rectangle-modal'></div>
+                    <div className='rectangle-modal2'></div>
+                    <div className='rectangle-modal3'></div>
+
+                    <p className='vagno'>Внимание</p>
+
+                    <p className='text-vagno3'>Ведутся технические работы! Задайте ваш вопрос менеджеру через чат</p>
+                
+                    <div className='button-ok'>
+                        <div className='rec-button' onClick={onClose}>Чат</div>        
+                    </div>
+                </div>
+            </MyModal>
 
         </div>
     );
